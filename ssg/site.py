15d5f70fa70338600abe55f989ff0c0f0d024025
JP Etcheber <jetcheber@gmail.com>
2021-07-25 01:07:23 -0700

    fixing tests

--- ssg/site.py
+++ ssg/site.py
@@ -1,16 +1,16 @@
-import Path from pathlib
+from pathlib import Path
 
 class Site:
     def __init__(self, source, dest):
         self.source = Path()
         self.dest = Path()
 
-    def create_dir():
+    def create_dir(self):
         directory = "{}/{}".format(self.dest, self.source.relative_to())
         directory.mkdir(parents=True, exist_ok=True)
 
-    def build():
+    def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
-                create_dir(path)
+                create_dir(self, path)
